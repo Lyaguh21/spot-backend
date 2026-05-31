76ed7ada31b5ed8e321423f6ae9ffe77c35b3726
LyaguhPC <artyom07206@gmail.com>
2026-05-31 13:13:54 +0300

    feat(main): fix cors to local network

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,54 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { ResponseInterceptor } from './common/interceptors/response.interceptor';
-import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
-import { AllExceptionFilter } from './common/filters/all-exeption.filter';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import cookieParser from 'cookie-parser';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { ResponseInterceptor } from "./common/interceptors/response.interceptor";
+import { LoggingInterceptor } from "./common/interceptors/logging.interceptor";
+import { AllExceptionFilter } from "./common/filters/all-exeption.filter";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(
-    new LoggingInterceptor(),
-  );
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new AllExceptionFilter());
 
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, 
-      forbidNonWhitelisted: true, 
-      transform: true, 
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
   const config = new DocumentBuilder()
-    .setTitle('API')
-    .setDescription('CRM API')
-    .setVersion('1.0')
-    .addCookieAuth('accessToken')
+    .setTitle("API")
+    .setDescription("CRM API")
+    .setVersion("1.0")
+    .addCookieAuth("accessToken")
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup("docs", app, document);
 
   app.enableCors({
     credentials: true,
-    origin: 'http://localhost:5173',
+    origin: (origin, callback) => {
+      if (!origin) {
+        callback(null, true);
+        return;
+      }
+
+      if (origin === "http://localhost:5173") {
+        callback(null, true);
+        return;
+      }
+
+      const isLanOrigin = /^http:\/\/\d{1,3}(?:\.\d{1,3}){3}:5173$/.test(
+        origin,
+      );
+      callback(null, isLanOrigin);
+    },
   });
 
   await app.listen(process.env.PORT ?? 3000);
