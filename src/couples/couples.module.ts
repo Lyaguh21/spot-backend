aac9e858068b874e06ca4b5f5c508375042f8284
LyaguhPC <artyom07206@gmail.com>
2026-06-30 14:08:44 +0300

    fixes

--- src/couples/couples.module.ts
+++ src/couples/couples.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CouplesService } from './couples.service';
 import { CouplesController } from './couples.controller';
 import { UsersModule } from 'src/users/users.module';
+import { StorageModule } from 'src/storage/storage.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, StorageModule],
   providers: [CouplesService],
   controllers: [CouplesController],
   exports: [CouplesService]
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { UsersService } from "./users.service";
 import { UsersController } from "./users.controller";
+import { StorageModule } from "src/storage/storage.module";
 
 @Module({
-  imports: [JwtModule.register({})],
+  imports: [JwtModule.register({}), StorageModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
